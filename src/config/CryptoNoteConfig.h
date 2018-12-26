66fffcf8af19d6fc63856caf7df9e97b93c446b6
affankhan43 <affankhan627@gmail.com>
2018-12-27 00:42:14 +0500

    algo

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -32,15 +32,15 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V3          = 11;
 
 // MONEY_SUPPLY - total number coins to be generated
 const uint64_t MONEY_SUPPLY                                  = UINT64_C(15000000000000);
-const uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX                   = 0;
-const size_t   ZAWY_DIFFICULTY_V2                            = 0;
+const uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX                   = 10000000000000;
+const size_t   ZAWY_DIFFICULTY_V2                            = 10000000000000;
 const uint8_t  ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION      = 3;
 
-const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX                 = 10000000;
-const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V2              = 10000000;
-const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V3              = 10000000;
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX                 = 10000000000000;
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V2              = 10000000000000;
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V3              = 10000000000000;
 
-const uint64_t LWMA_3_DIFFICULTY_BLOCK_INDEX                 = 10000000;
+const uint64_t LWMA_3_DIFFICULTY_BLOCK_INDEX                 = 10000000000000;
 
 const unsigned EMISSION_SPEED_FACTOR                         = 25;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
@@ -148,9 +148,9 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 
 const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
 
-const uint32_t UPGRADE_HEIGHT_V2                             = 1;
-const uint32_t UPGRADE_HEIGHT_V3                             = 100000000;
-const uint32_t UPGRADE_HEIGHT_V4                             = 100000000; // Upgrade height for CN-Lite Variant 1 switch.
+const uint32_t UPGRADE_HEIGHT_V2                             = 10000000000000;
+const uint32_t UPGRADE_HEIGHT_V3                             = 10000000000000;
+const uint32_t UPGRADE_HEIGHT_V4                             = 10000000000000; // Upgrade height for CN-Lite Variant 1 switch.
 const uint32_t UPGRADE_HEIGHT_CURRENT                        = UPGRADE_HEIGHT_V4;
 
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
